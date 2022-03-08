fe4e1958134faca3a57a6a367640c915cebda2e5
achan81 <achan81@icloud.com>
2022-03-08 15:34:06 +0000

    basic functions built - now to fix bugs and add delete function

--- README.md
+++ README.md
@@ -12,3 +12,13 @@ To prevent the `failed-to-compile` issue for linter errors like `no-unsed-vars`,
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+### key learnings
+how to build a to-do-list
+
+### UUID 
+- A universally unique identifier (UUID) is a 128-bit format for creating IDs in code that has become popular in recent years, especially in relation to database keys. By using UUIDs, you ensure that your ID is not just unique in the context of a single database table or web application, but is truly unique in the universe. 
+- All UUIDs are 128 bits in length, but are commonly represented as 32 hexadecimal characters separated by four hyphens.
+- This app uses Version 4 UUIDs.
+- It is important to note that while the probability that a UUID will collide with another is not zero, its practically zero. The chances of collision are so astronomically low, worrying about it would be ridiculous. The total number of possible UUIDs is ```2^128``` or ```340282366920938463463374607431768211456.```
+
+Further reading: https://qvault.io/clean-code/what-are-uuids-and-should-you-use-them/
--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
+import React from 'react'
+import { v4 as uuid } from 'uuid'
+
 function App() {
-  return <h1>Hello World</h1>
+  const [todos, setTodos] = React.useState([ 
+  // { task: 'Build a todo app', completed: false, id: uuid() }
+  ])
+  
+  const [newTodo, setNewTodo] = React.useState('') // console.log('current todos', todos)
+
+  const handleSubmit = (e) => {
+    e.preventDefault() // console.log('submitting', newTodo)
+    const createdTodo = {
+      task: newTodo,
+      completed: false,
+      id: uuid(),
+    }
+    setTodos([...todos, createdTodo]) //add todo to the end of the list
+    setNewTodo('') //this will revert the input box back to its original start point.
+  }
+
+  const handleChange = (e) => { // console.log('typing', e.target.value)
+    setNewTodo(e.target.value)
+  }
+
+  return (
+    <main>
+      <h1>You have {todos.length} thing(s) to do!</h1> 
+      {/* rerendered to show how my Todos there are */}
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.task}</li>
+        ))}
+      </ul>
+
+      <form onSubmit={handleSubmit}>
+        <input placeholder="Task" onChange={handleChange} value={newTodo} />
+        {/* controlled input - by adding value key*/}
+        <button>Add</button>
+      </form>
+    </main>
+  )
 }
 
 export default App
